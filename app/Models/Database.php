d5c989440888d430bb15c4c241daaeb2ab481f37
Rodolphe <dutchr@live.fr>
2024-11-22 10:43:53 +0100

    :wrench: Works !

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -1,14 +1,22 @@
 <?php
+namespace App\Models;
+
+use PDO;
+use PDOException;
+
 class Database {
-    private static $instance = null;
+    private static $connection = null;
 
     public static function getConnection() {
-        if (!self::$instance) {
-            $config = include(__DIR__ . '/../../config/database.php');
-            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
-            self::$instance = new PDO($dsn, $config['user'], $config['password']);
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if (!self::$connection) {
+            try {
+                $dsn = "mysql:unix_socket={$_ENV['DB_SOCKET']};dbname={$_ENV['DB_NAME']};charset={$_ENV['DB_CHARSET']}";
+                self::$connection = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Erreur de connexion : " . $e->getMessage());
+            }
         }
-        return self::$instance;
+        return self::$connection;
     }
 }
---
+++ app/Views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accueil RPG</title>
+</head>
+<body>
+    <h1>Bienvenue sur le RPG Game App</h1>
+
+</body>
+</html>
